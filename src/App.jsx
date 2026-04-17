77e4aae738c9f42f0bc78a6ee3d4566e972045b7
srthkksrwni <srthkksrwnicool@gmail.com>
2026-04-17 18:09:53 +0530

    major changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-// HashRouter aur useLocation ko import kiya
-import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom"; 
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -23,57 +22,11 @@ import Events from "./components/Events";
 import Contact from "./components/Contact";
 import StudentCorner from "./components/StudentCorner";
 import ModestIframe from "./components/ModestIframe";
-import OntologyEntry from "./ontology/OntologyEntry";
 
-// Ek naya component banaya taaki location track ho sake
-function AppContent() {
-  const location = useLocation();
 
-  // Ye check karega ki kya hum ontology waale pages par hain
-  // Agar path "/ontology" se start hota hai toh ye true hoga
-  const isOntologyProject = location.pathname.startsWith("/ontology");
-
-  return (
-    <div className="App">
-      <ScrollToTop />
-      
-      {/* Agar ontology project NAHI hai, tabhi Project A ka Navbar dikhao */}
-      {!isOntologyProject && <Navbar />}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/faculty" element={<Faculty />} />
-        <Route path="/students" element={<Students />} />
-        <Route path="/publications" element={<Publications />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/resources" element={<ResourcesPage />} />
-        <Route path="/datasets" element={<DatasetsPage />} />
-        <Route path="/projectpage" element={<ProjectPage />} />
-        <Route path="/gesturecare" element={<BimodalGestureCareWebsite />} />
-        <Route path="/healthmonitor" element={<HealthMonitoringProject />} />
-        <Route path="/cepproject" element={<CEPProject />} />
-        <Route path="/ontologyproject" element={<OntologyProject />} />
-        <Route path="/cloudplatform" element={<CloudPlatformProject />} />
-        <Route path="/mobile-app" element={<MobileApplicationProject />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/studentCorner" element={<StudentCorner />} />
-        <Route path="/modest" element={<ModestIframe />} />
-        
-        {/* Project B ka rasta */}
-        <Route path="/ontology/*" element={<OntologyEntry />} />
-      </Routes>
-
-      {/* Agar ontology project NAHI hai, tabhi Project A ka Footer dikhao */}
-      {!isOntologyProject && <Footer />}
-    </div>
-  );
-}
-
-// Main App component
 function App() {
   return (
+  
     <Router>
       <div className="App">
         <ScrollToTop />
@@ -98,7 +51,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/studentCorner" element={<StudentCorner />} />
           <Route path="/modest" element={<ModestIframe />} />
-          
+           
         </Routes>
         <Footer />
       </div>
