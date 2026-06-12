e892a37feed6a8b1d5d7d070104c75d0185f71d3
singhanandita09 <ananditasin17@gmail.com>
2026-06-12 16:40:07 +0530

    added filelink

--- src/data/postdoc.js
+++ src/data/postdoc.js
@@ -3,7 +3,7 @@ export const postdoc = [
     name: "Dr. Sadhana Tiwari",
     email: "sadhanatiwari.22@gmail.com \n prf.sadhana@iiita.ac.in",  
     scholarLink: "https://scholar.google.com/citations?user=zv8vatkAAAAJ&hl=en&oi=ao",
-    fileLink: "https://iiita.ac.in/faculty/sadhana-tiwari",
+    fileLink: "/#/portfolio",
     img: "../../Sadhana Tiwari.jpg"
   }
 ];
