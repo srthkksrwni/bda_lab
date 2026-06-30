8814ded0318b0d101280708af5d3049e04340537
kartik1525 <kartikeyasingh225@gmail.com>
2026-06-30 16:08:48 +0530

    feat: initialize database connection and configuration files for the backend

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,17 +1,23 @@
 <?php
+$config = require __DIR__ . "/config.php";
 
+// header("Access-Control-Allow-Origin: " . $config["FRONTEND_URL"]);
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
-$conn = new mysqli("localhost", "root", "", "bda_lab");
+$conn = new mysqli(
+    $config["DB_HOST"],
+    $config["DB_USER"],
+    $config["DB_PASS"],
+    $config["DB_NAME"]
+);
 
 if ($conn->connect_error) {
-    http_response_code(500);
-    echo json_encode([
+
+    die(json_encode([
         "success" => false,
-        "message" => "Database connection failed: " . $conn->connect_error
-    ]);
-    exit();
+        "message" => "Database connection failed"
+    ]));
 }
