297de833a6123a8f13dab5639e32516e0d58cf42
kartik1525 <kartikeyasingh225@gmail.com>
2026-07-07 17:31:34 +0530

    feat: add AdminStudents component to manage student records and categories

--- src/admin/People/AdminStudents/Adminstudents.jsx
+++ src/admin/People/AdminStudents/Adminstudents.jsx
@@ -224,6 +224,21 @@ return (
         value={batchYear}
         onChange={(e) => setBatchYear(e.target.value)}
       >
+        <option value="2045">2045</option>
+        <option value="2044">2044</option>
+        <option value="2043">2043</option>
+        <option value="2042">2042</option>
+        <option value="2041">2041</option>
+        <option value="2040">2040</option>
+        <option value="2039">2039</option>
+        <option value="2038">2038</option>
+        <option value="2037">2037</option>
+        <option value="2036">2036</option>
+        <option value="2035">2035</option>
+        <option value="2034">2034</option>
+        <option value="2033">2033</option>
+        <option value="2032">2032</option>
+        <option value="2031">2031</option>
         <option value="2030">2030</option>
         <option value="2029">2029</option>
         <option value="2028">2028</option>
